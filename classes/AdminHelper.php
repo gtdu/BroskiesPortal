f1ce6093430494f87fad5d3fab981f9c18ca3a7f
Ryan Cobelli <ryan.cobelli@rybel-llc.com>
2020-01-07 23:22:26 -0500

    Send welcome emails

--- classes/AdminHelper.php
+++ classes/AdminHelper.php
@@ -1,6 +1,9 @@
 <?php
 
 use Ramsey\Uuid\Uuid;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
 class AdminHelper
 {
@@ -34,7 +37,52 @@ class AdminHelper
         $handle->bindValue(1, $name);
         $handle->bindValue(2, $email);
         $handle->bindValue(3, password_hash($password, PASSWORD_DEFAULT));
-        return $handle->execute();
+        if ($handle->execute()) {
+            $this->sendWelcomeEmail($email, $password);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function sendWelcomeEmail($email, $password) {
+        $handle = $this->conn->prepare('SELECT id FROM users WHERE email = ? LIMIT 1');
+        $handle->bindValue(1, $email);
+        $handle->execute();
+        $result = $handle->fetchAll(\PDO::FETCH_ASSOC);
+
+        if (empty($result)) {
+            return;
+        }
+
+        // Instantiation and passing `true` enables exceptions
+        $mail = new PHPMailer(true);
+
+        try {
+            //Server settings
+            // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+            $mail->isSMTP();                                            // Send using SMTP
+            $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+            $mail->Username   = 'ryan.cobelli@gmail.com';               // SMTP username
+            $mail->Password   = $this->config['email_password'];                     // SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+            $mail->Port       = 587;                                    // TCP port to connect to
+
+            //Recipients
+            $mail->setFrom('broskies@gtdu.org', 'Broskies Portal');
+            $mail->addAddress($email);
+            $mail->addReplyTo('webmaster@gtdu.org', 'Webmaster');
+
+            // Content
+            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->Subject = 'Welcome To Broskies Portal!';
+            $mail->Body    = "Welcome to Broskies Portal, where you can access a bunch of useful shit in one place. Please login at: https://broskies.gtdu.org/. Your password is " . $password . ". If you have any questions, please respond to this email.";
+
+            $mail->send();
+        } catch (Exception $e) {
+            exit("Message could not be sent. Mailer Error: {$mail->ErrorInfo}");
+        }
     }
 
     // Modify user permissions
