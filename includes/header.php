ab647788a3a2f0c02f7ae0b34c2c675d005fa580
Ryan Cobelli <ryan.cobelli@rybel-llc.com>
2020-02-04 12:29:18 -0500

    Add favicon

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="../resources/layout.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="../resources/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../resources/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../resources/favicon/favicon-16x16.png">
+    <link rel="manifest" href="../resources/favicon/site.webmanifest">
+    <link rel="mask-icon" href="../resources/favicon/safari-pinned-tab.svg" color="#3d7cff">
+    <link rel="shortcut icon" href="../resources/favicon/favicon.ico">
+    <meta name="msapplication-TileColor" content="#3d7cff">
+    <meta name="msapplication-config" content="../resources/favicon/browserconfig.xml">
+    <meta name="theme-color" content="#3d7cff">
 </head>
 
 <body>
