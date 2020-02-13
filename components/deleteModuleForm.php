99a0bad36b33391a3fd7b862cce257e4e5f22154
Ryan Cobelli <ryan.cobelli@rybel-llc.com>
2020-02-13 12:50:38 -0500

    Fix delting modules

--- components/deleteModuleForm.php
+++ components/deleteModuleForm.php
@@ -4,7 +4,7 @@
         <div class="form-group">
             <label for="input1">Are you sure you want to delete this module?</label>
         </div>
-        <input type="hidden" name="resource_id" value="<?php echo $item['id']; ?>">
+        <input type="hidden" name="module_id" value="<?php echo $item['id']; ?>">
         <input type="hidden" name="action" value="deleteModule">
         <button type="submit" class="btn btn-danger">Yes</button>
         <a href="?" class="btn btn-secondary active" role="button" aria-pressed="true">No</a>
