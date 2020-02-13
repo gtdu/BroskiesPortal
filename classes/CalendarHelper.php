f1beae5bc27d557468d12980e6586f37007e9012
Ryan Cobelli <ryan.cobelli@rybel-llc.com>
2020-02-13 12:50:26 -0500

    Make calendar look better

--- classes/CalendarHelper.php
+++ classes/CalendarHelper.php
@@ -12,21 +12,34 @@ class CalendarHelper extends Helper
 
     public function renderCalendar() {
         try {
+            // Pull events from ical feed
             $ical = new ICal();
             $ical->initUrl($this->config['cal_url'], $username = null, $password = null, $userAgent = null);
 
-            $events = $ical->eventsFromInterval('1 week');
-
-            foreach ($events as $event) {
-                $dtstart = $ical->iCalDateToDateTime($event->dtstart_array[3]);
-
-                $output = '<b>' . $dtstart->format('m/d/Y') . "</b> " . $event->summary;
-                if (!empty($event->description)) {
-                    $output .= ": " . $event->description;
-                }
-                $output .=  "</br>";
-                echo $output;
+            echo "<table class='table'>";
+            echo "<tr>";
+            // Days of the week are headers
+            for ($i = 0; $i < 7; $i++) {
+                echo "<th>" . date('l', strtotime('+' . $i . ' days')) . "</th>";
             }
+            echo "</tr>";
+
+            for ($i = 0; $i < 7; $i++) {
+                echo "<td>";
+                // Pull events on that day only
+                $events = $ical->eventsFromRange($i . ' day', ($i) . ' day');
+                // Check if there is an event
+                if (!empty($events)) {
+                    // Render each event
+                    foreach ($events as $event) {
+                        echo $event->summary . "</br>";
+                    }
+                } else {
+                    echo "<i>Nothing</i>";
+                }
+                echo "</td>";
+            }
+            echo "</table>";
         } catch (\Exception $e) {
             die($e);
         }
